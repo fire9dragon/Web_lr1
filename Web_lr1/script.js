f1b3b8e9851e53e2dbff9aa841a70cad5c570dc8
Nyx864 <kebets19952@gmail.com>
2025-02-18 09:51:13 +0300

    update project

--- Web_lr1/script.js
+++ Web_lr1/script.js
@@ -16,3 +16,25 @@ popupText.addEventListener("transitionend", () => {
     popupText.style.height = "auto";
   }
 });
+
+cards = document.getElementsByClassName("product-card");
+
+Array.from(cards).forEach(function (card) {
+  const characters = card.querySelector(".product-characters");
+
+  card.addEventListener("mouseover", () => {
+    characters.style.height = `${characters.scrollHeight}px`;
+  });
+
+  card.addEventListener("mouseout", () => {
+    characters.style.height = `${characters.scrollHeight}px`;
+    window.getComputedStyle(characters, null).getPropertyValue("height");
+    characters.style.height = "0";
+  });
+
+  characters.addEventListener("transitionend", () => {
+    if (characters.style.height !== "0px") {
+      characters.style.height = "auto";
+    }
+  });
+});
